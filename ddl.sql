404456134e594ab1d12539ae48b53ca9608718a6
MSIM2014 <wcao@uw.edu>
2016-04-17 23:39:17 -0700

    ddl

---
+++ ddl.sql
@@ -0,0 +1,8 @@
+create table course_catalogue 
+(date_of_scrape Date, 
+university varchar(100), 
+department varchar(100), 
+course_number varchar(100), 
+course_title varchar(100),
+prerequisite varchar(200)
+);
